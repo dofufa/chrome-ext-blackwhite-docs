520199806c0c7da0f5df49d1b6238d6df3a74917
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2024-02-19 08:31:19 -0800

    Update ext.js

--- src/js/ext.js
+++ src/js/ext.js
@@ -1,1 +1,12 @@
 console.log('ext-button-hello-ext');
+
+ext();
+
+function ext() {
+	d = document;
+	q = d.querySelectorAll('*');
+	q.forEach((a) => {
+		a.style.color='black';
+		a.style.backgroundColor = 'white';
+	});
+}
